34cfed6d6f1e01066cbcdbba9bed6e9e63b2e8ab
akadroid <huynhdanny1@gmail.com>
2023-11-19 22:04:41 -0800

    updated dots

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -8,7 +8,8 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0;\
+		keyword windowrulev2 immediate,class:(exe)|(genshinimpact)|(lutris)|(leagueclientux.exe)|(league of legends.exe);"
     exit
 fi
 hyprctl reload
