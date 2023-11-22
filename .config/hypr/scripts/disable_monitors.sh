728bb35c5af8f87cbcb820c6b99d2618197364b5
akadroid <huynhdanny1@gmail.com>
2023-11-22 09:54:38 -0800

    new dots

--- .config/hypr/scripts/disable_monitors.sh
+++ .config/hypr/scripts/disable_monitors.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-if hyprctl monitors | grep -q HDMI-A-1; then
+if hyprctl monitors | grep -q DP-1; then
     hyprctl --batch "\
-        keyword monitor HDMI-A-1,disable"
+        keyword monitor DP-1,disable"
 else
 	hyprctl reload
 fi
--- .config/nvim/init.vim
+++
@@ -1,4 +0,0 @@
-set clipboard+=unnamedplus
-set tabstop=4
-set ts=4 sw=4
-set nowrap
